98e38e8bc1f50ec5a2bb771e33d99633be34a833
Avissonitus <104322532+Avissonitus@users.noreply.github.com>
2022-05-22 17:50:52 +0200

    konfiguracja mikrokontrolera projekt

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+//#include "p32mk1024mcm064.h"
+#include "DeviceConfig.h"
+
+int main(){
+
+    while(1){
+        
+    }
+    return 0;
+}
